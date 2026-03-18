e0c20d6c5e23c0fbdb436e916864b97d313fe714
Andreas <au645389@uni.au.dk>
2026-03-18 22:10:21 +0100

    Add nvim-navic for LSP code context display

--- lua/smakkedak/lazy/treesitter.lua
+++ lua/smakkedak/lazy/treesitter.lua
@@ -1,17 +1,29 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  lazy = false,
-  config = function()
-    local configs = require("nvim-treesitter")
- 
-    configs.setup({
-      ensure_installed = {
-        "rust","c", "lua", "vim", "vimdoc", "elixir", "javascript", "html", "python", "typescript"
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			local treesitter = require("nvim-treesitter")
+
+			-- Enable Treesitter highligh to all filetypes that there are parsers for.
+			vim.api.nvim_create_autocmd('FileType', {
+				callback = function(args)
+					if
+					-- is arg2 in arg1
+						vim.list_contains(
+						-- all installed parsers
+							treesitter.get_installed(),
+							-- get parser for current language
+							vim.treesitter.language.get_lang(args.match)
+						)
+					then
+						vim.treesitter.start(args.buf)
+					end
+				end,
+			})
+
+			-- Treesitter indentation
+			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		end
 }

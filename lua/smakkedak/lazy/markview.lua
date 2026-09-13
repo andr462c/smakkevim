5963eea48b7a0becc3d42d9b5a7924acdd634378
Andreas <au645389@uni.au.dk>
2026-09-13 11:01:49 +0200

    markdown

--- lua/smakkedak/lazy/markview.lua
+++ lua/smakkedak/lazy/markview.lua
@@ -1,7 +1,6 @@
 return {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
-
-    -- Completion for `blink.cmp`
-    dependencies = { "saghen/blink.cmp" },
-};
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+}
--- lua/smakkedak/remap.lua
+++ lua/smakkedak/remap.lua
@@ -78,6 +78,8 @@ local maps = {
   { "<C-j>",       "<C-w>j",                                                                             desc = "Go to Lower Window" },
   { "<C-k>",       "<C-w>k",                                                                             desc = "Go to Upper Window" },
   { "<C-l>",       "<C-w>l",                                                                             desc = "Go to Right Window" },
+  -- Markdown
+  { '<C-m>', ':MarkdownPreviewToggle<CR>', desc = "Toggle markdown preview"},
 }
 
 for _, m in ipairs(maps) do
